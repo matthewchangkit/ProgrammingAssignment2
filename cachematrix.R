354b70545ede059d3557be7778899209074076b8
Matthew Chang Kit <matthewchangkit@outlook.com>
2015-04-25 19:33:43 -0700

    Successful creation of caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Caching the inverse of a matrix
 
-## Write a short comment describing this function
+## Matrix invertibility can be a very time consuming job when
+## they are either:
+## 1) Very large matrices; or
+## 2) Multiple matrices wrapped in an array, list, etc.
+##
+## As such, we address this issue by creating functions which
+## stores these matrices and "prepare" them to be invertible,
+## and then create the invertible matrices from these 
+## stored "prepared" matrices. Below we shall briefly overview
+## what each function is doing. 
 
-makeCacheMatrix <- function(x = matrix()) {
 
+### The function below creates a special matrix object that
+### can cache the inverse of the matrix
+
+makeCacheMatrix <- function(x=matrix()) {
+  inv.x <- NULL
+  set <- function(y) {
+    x <<- y
+    inv.x <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv.x <<- inverse
+  getinverse <- function() inv.x
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+### The function below computes the inverse of the special 
+### matrix returned by makeCacheMatrix above. If the inverse
+### has already been calculated (and the matrix has not 
+### changed) then the cachesolve should retrieve the 
+### inverse from the cache.
 
-## Write a short comment describing this function
+#### We shall assume that the matrix being input is always
+#### invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv.x <- x$getinverse()
+  if(!is.null(inv.x)) {
+    message("obtaining cached inverse matrix")
+    return(inv.x)
+  }
+  else {
+    inv.x <- inverse(x$get(), ...)
+    x$setinverse(inv.x)
+    return(inv.x)
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
